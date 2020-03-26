59836a9b9f84bc39122489b4f24b464176b4ac82
EdgarAlatalo <62548576+EdgarAlatalo@users.noreply.github.com>
2020-03-26 10:01:37 +0200

    Updates

---
+++ precache-manifest.03ac56eb01f83eefe47596a43df0ee63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d85fdc2e5cb3a207e63e4c55e97a8b2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "233791426abf1947f32c",
+    "url": "/my-app/static/css/main.1bdf3e52.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/my-app/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233791426abf1947f32c",
+    "url": "/my-app/static/js/main.0b382f89.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  }
+]);
